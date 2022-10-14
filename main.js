d11cf5be2f7874d33ea058ef09767b46d5968d6f
pan78m <103315572+pan78m@users.noreply.github.com>
2022-10-14 13:23:55 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    //Sticky menu
+    $(".js--services-section").waypoint(function(direction){
+        if (direction=="down") {
+            $("nav").addClass("sticky");
+        }
+        else {
+            $("nav").removeClass("sticky"); 
+        }
+
+    });
+ 
+    
+    
+    
+//     var $navbar = $('.navbar');
+//     $navbar.waypoint(function () {
+//         if (direction=='down') {
+//             $navbar.addClass('sticky'); 
+//         }
+//         else {
+//             $navbar.removeClass('sticky');
+//         }
+       
+//     },
+//  { offset: '20%' 
+//     });
+
+
+
+
+ 
+
+    //mixeup (portfolio section)//
+    var mixer = mixitup('.container');
+});
